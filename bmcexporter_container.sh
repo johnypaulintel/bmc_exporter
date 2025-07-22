bc45f89a28a3d8f37550e5a3ad1c2f4ca89f8ac1
Johny Paul <johny.paul@intel.com>
2025-07-22 01:05:23 -0700

    Updated exporter to red-fish and also added settings in prometheus to read from both bmc exporter and metric exporter

--- bmcexporter_container.sh
+++ bmcexporter_container.sh
@@ -1,2 +1,2 @@
 docker rm -f bmc-exporter
-docker run --name bmc-exporter -d --network host -p 5000:5000 -v $PWD/bmc_exporter:/tmp vault.habana.ai/habana-bmc-exporter/bmc-exporter:1.21.2-76 -config /tmp/bmc_exp_config.json -daemon-set false --exporter='g3-red-fish'
+docker run --name bmc-exporter -d --network host -p 5000:5000 -v $PWD/bmc_exporter:/tmp vault.habana.ai/habana-bmc-exporter/bmc-exporter:1.21.2-76 -config /tmp/bmc_exp_config.json -daemon-set false --exporter='red-fish'
