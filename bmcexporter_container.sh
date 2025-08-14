db459814854381c6dcd75af3feee9e1bcc4fe994
Kirill Priadko <kirill.priadko@intel.com>
2025-08-14 10:56:52 -0700

    SMC BMC support

--- bmcexporter_container.sh
+++ bmcexporter_container.sh
@@ -1,2 +1,2 @@
 docker rm -f bmc-exporter
-docker run --name bmc-exporter -d --network host -p 5000:5000 -v $PWD/bmc_exporter:/tmp vault.habana.ai/habana-bmc-exporter/bmc-exporter:1.21.2-76 -config /tmp/bmc_exp_config.json -daemon-set false --exporter='red-fish'
+docker run --name bmc-exporter -d --network host -p 5000:5000 -v $PWD/bmc_exporter/config.json:/app/config.json -v $PWD/bmc_exporter/smcredfish.yaml:/etc/g3redfish.yaml vault.habana.ai/habana-bmc-exporter/bmc-exporter:1.21.2-76 -config /app/config.json -exporter='g3-red-fish' -daemon-set=false
